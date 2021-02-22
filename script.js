9d62560d7a65556ea95dbdd612728c837da82efb
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-02-22 15:34:33 +0100

    core structure

---
+++ script.js
@@ -0,0 +1,36 @@
+const recipesList = document.getElementById("recipes-list");
+const input = document.getElementById("input");
+let value = "fish";
+
+let API_URL; //= `https://api.edamam.com/search?q=${value}&app_id=ae955ef4&app_key=ede746169d09b2dacf6c78ef642dbf97&from=0&to=15&calories=591-722&health=alcohol-free`;
+
+const fetchData = () => {
+  fetch(API_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log("recipe list", json);
+      json.hits.forEach((item) => {
+        // console.log(item);
+        recipesList.innerHTML += `
+            <li>
+                ${item.recipe.label}
+                <a href="${item.recipe.url}">
+                    <img src="${item.recipe.image}"/>
+                </a>
+                <div> This dish is from: ${item.recipe.source}</div>
+                <div>Total time: ${item.recipe.totalTime} minutes</div>
+            </li>
+        `;
+      });
+    });
+};
+
+const changeHandeler = () => {
+  value = input.value;
+  API_URL = `https://api.edamam.com/search?q=${value}&app_id=ae955ef4&app_key=ede746169d09b2dacf6c78ef642dbf97&from=0&to=15&calories=591-722&health=alcohol-free`;
+  console.log(value);
+  recipesList.innerHTML = "";
+  fetchData();
+};
