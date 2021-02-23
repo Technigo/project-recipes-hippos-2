3fee9fcaa4e8ac9ce1a07a68faee6ea0308a3399
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-02-23 10:41:10 +0100

    sorting fixed inside of fetch, code cleaning

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const recipesList = document.getElementById("recipes-list");
 const recipesListF = document.getElementById("recipes-listfilter");
-const recipesListFS = document.getElementById("recipes-listsortedfilter");
-
 
 const input = document.getElementById("input");
-//let value = "fish";
 
-
+//////////******************** */
 const cookingTime = 20;
 
-let recipeExtract=[],
-dish, newRecipe;
-
-let API_URL; //= `https://api.edamam.com/search?q=${value}&app_id=ae955ef4&app_key=ede746169d09b2dacf6c78ef642dbf97&from=0&to=15&calories=591-722&health=alcohol-free`;
+let recipeExtract=[];
+let API_URL; 
 
 const fetchData = () => {
   fetch(API_URL)
@@ -24,12 +19,10 @@ const fetchData = () => {
       recipeExtract = json.hits
       console.log("extract", recipeExtract)
 
-      json.hits.forEach((item) => {
-        // console.log(item);
-
-
-        //+if (item.recipe.totalTime < cookingTime) {
-        
+      recipeExtract.sort((a ,b) => {
+        return a.recipe.totalTime - b.recipe.totalTime
+                 
+        }).forEach((item) => { 
         recipesList.innerHTML += `
             <li>
                 ${item.recipe.label}
@@ -40,32 +33,16 @@ const fetchData = () => {
                 <div>Total time: ${item.recipe.totalTime} minutes</div>
             </li>
         `;
-
-       //+ }   
-        
       }); 
       filterCookingTime()
-       
     });
-    
-    
-    
-  
-    
 };
 
 /*********************FILTER ************************/
 const filterCookingTime = () => {
-  
-  console.log("check1")
   recipesListF.innerHTML = ""
-
   recipeExtract.forEach((item) => {
-    
-    //recipesListF.innerHTML = ""
-
     if (item.recipe.totalTime < cookingTime) {
-    
       recipesListF.innerHTML += `
         <li>
           ${item.recipe.label}
@@ -80,43 +57,7 @@ const filterCookingTime = () => {
       //do nothing
     }
   }) 
-  
 } 
-
-
-
-
-/********************SORTING ****************/
-//let newList = []
-
-recipeExtract.sort((a ,b) => {
-  return a.recipe.totalTime - b.recipe.totalTime
-    
-  }) 
-
-
-  
-
-  
-    
-  
-
-   
-  
-
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
 
 const changeHandeler = () => {
   value = input.value;
