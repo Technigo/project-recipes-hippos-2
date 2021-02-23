d6b2d91f7cdd652120f8075a5f46edb2154c2244
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-02-23 12:35:05 +0100

    add style

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const recipesList = document.getElementById("recipes-list");
 const input = document.getElementById("input");
+const headerImage = document.getElementById("header-img");
+const headerTitle = document.getElementById("header-title");
 let value = "fish";
 
 let API_URL; //= `https://api.edamam.com/search?q=${value}&app_id=ae955ef4&app_key=ede746169d09b2dacf6c78ef642dbf97&from=0&to=15&calories=591-722&health=alcohol-free`;
@@ -15,12 +17,12 @@ const fetchData = () => {
         // console.log(item);
         recipesList.innerHTML += `
             <li>
-                ${item.recipe.label}
-                <a href="${item.recipe.url}">
-                    <img src="${item.recipe.image}"/>
+                <a href="${item.recipe.url}" class="recipe-image">
+                    <img src="${item.recipe.image}" class="responsive-img" />
                 </a>
-                <div> This dish is from: ${item.recipe.source}</div>
-                <div>Total time: ${item.recipe.totalTime} minutes</div>
+                <h5 class="recipe-title">${item.recipe.label}</h5>
+                <div class="recipe-info"> This dish is from: ${item.recipe.source}</div>
+                <div class="recipe-info">Total time: ${item.recipe.totalTime} minutes</div>
             </li>
         `;
       });
@@ -32,5 +34,9 @@ const changeHandeler = () => {
   API_URL = `https://api.edamam.com/search?q=${value}&app_id=ae955ef4&app_key=ede746169d09b2dacf6c78ef642dbf97&from=0&to=15&calories=591-722&health=alcohol-free`;
   console.log(value);
   recipesList.innerHTML = "";
+  headerImage.style.height = "50vh";
+  headerImage.style.transition = "height 0.5s ease-in";
+  headerTitle.style.display = "block";
   fetchData();
+  input.value = "";
 };
